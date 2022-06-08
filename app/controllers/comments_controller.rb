367742511c6a0b6e555770dc1270b457655ad73b
Charan <divamdinnasai@gmail.com>
2022-06-08 11:14:31 +0530

    init

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,20 @@
 class CommentsController < ApplicationController
-    def create
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.create(comment_params)
-      redirect_to article_path(@article)
-    end
-  
-    private
+  http_basic_authenticate_with name: "blog", password: "digiwabbit", only: :destroy
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: 303
+  end
+
+  private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
     end
-
-  end
-  
+end
